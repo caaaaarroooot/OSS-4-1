6977f24f29b3567b174b0885787594826acc7258
Jiyun <12jiyun@gmail.com>
2024-09-28 19:00:10 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import jiyun from "./assets/지윤.jpeg";
+import "./App.css";
+import styled from "styled-components";
+import Hello from "./components/hello";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <MotherDiv>
+            <header className="App-header">
+                <img src={jiyun} className="App-logo" alt="logo" />
+                <img src={jiyun} style={{ width: "170px" }} alt="jiyun" />
+
+                <Hello />
+                <p>
+                    <span style={{ color: "red", fontSize: "35px" }}>I Edited</span> <code>src/App.js</code> and saved
+                    to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </MotherDiv>
+    );
 }
+
+const MotherDiv = styled.div`
+    width: 100vw;
+    height: 100vh;
+`;
 
 export default App;
---
+++ src/components/hello.js
@@ -0,0 +1,6 @@
+import "./hello.css";
+
+function Hello() {
+    return <h1>hello world!!!</h1>;
+}
+export default Hello;
